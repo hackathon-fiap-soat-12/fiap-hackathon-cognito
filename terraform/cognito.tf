437e4fd5a926b4f0c21bb19732b9cf3d02a5ba7b
DiguyaDeveloper <diegocecconcwb@outlook.com>
2025-04-10 20:09:52 -0300

    feat: update terraform cognito

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -33,15 +33,15 @@ resource "aws_cognito_resource_server" "gateway_scope" {
 resource "aws_cognito_user_pool_client" "user_pool_client" {
   name            = "fiap-hackathon-client"
   user_pool_id    = aws_cognito_user_pool.user_pool.id
-  generate_secret = true
+  generate_secret = false
 
   allowed_oauth_flows_user_pool_client = true
-  allowed_oauth_flows                  = ["implicit"]
-  allowed_oauth_scopes                 = ["email", "openid"]
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_scopes                 = ["email", "openid", "profile"]
 
-  prevent_user_existence_errors = "ENABLED"
+#  prevent_user_existence_errors = "ENABLED"
   supported_identity_providers  = ["COGNITO"]
-  explicit_auth_flows           = ["ALLOW_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
+#  explicit_auth_flows           = ["ALLOW_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
 
   callback_urls = ["https://fiap-soat-12.github.io/fiap-soat-fiap-hackathon-api/"]
 }
